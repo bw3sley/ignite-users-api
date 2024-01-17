591a79b1fe71f7957d39c7cc14b673e3d5b1fc25
Wesley Bernardes <bw3sley@gmail.com>
2024-01-17 03:15:22 -0300

    :tada: chore: initial commit

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import http from "node:http";
+
+import { json } from "./middlewares/json.js";
+
+import { routes } from "./routes.js";
+
+import { extractQueryParams } from "./utils/extract-query-params.js";
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req;
+
+    await json(req, res);
+
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url);
+    })
+
+    if (route) {
+        const routeParams = req.url.match(route.path);
+
+        const { query, ...params } = routeParams.groups;
+
+        console.log(routeParams, query);
+ 
+        req.params = params;
+        req.query = query ? extractQueryParams(routeParams.groups.query) : {};
+
+        return route.handler(req, res);
+    }
+    
+    return res.writeHead(404).end();
+})
+
+server.listen(3333, () => console.log("HTTPs server running at 3333..."));
